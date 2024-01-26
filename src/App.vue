9a4793b044aa0e1be2957262c4c7f957100c3129
MarcoGotti <mrc.gotti@gmail.com>
2024-01-26 20:25:29 +0000

    commit exercize 2

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,42 @@
 export default {
   name: "App",
   data(){
-
+    return {
+      list: [
+        { 
+          todo:'spesa',
+          done: true
+        },
+        { 
+          todo:'meccanico',
+          done: true
+        },
+        { 
+          todo:'giornale',
+          done: false
+        },
+        { 
+          todo:'posta',
+          done: true
+        },
+        { 
+          todo:'giardino',
+          done: true
+        }
+      ]
+    }
   }
 }
 </script>
 
 <template>
   <div>
-   nope 
+   <template v-for="item in list">
+    <li v-if="item.done">
+      {{ item.todo }}
+    </li>
+    <li v-else></li>
+   </template>
     
   </div>
 
